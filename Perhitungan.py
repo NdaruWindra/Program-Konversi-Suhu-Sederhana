78b9b53aa043cbc695b683aabc813f8e382a114d
Ndaru Windra Sayidinaa <88068972+NdaruWindra@users.noreply.github.com>
2022-11-07 09:58:51 +0700

    Test Aritmatika Sederhana

---
+++ Perhitungan.py
@@ -0,0 +1,14 @@
+Nilai_a = int(input("Masukan Nilai A : "))
+Nilai_b = int(input("Masukan Nilai B : "))
+
+tambah = Nilai_a + Nilai_b
+kurang = Nilai_a - Nilai_b
+kali = Nilai_a * Nilai_b
+bagi = Nilai_a / Nilai_b
+modulus = Nilai_a % Nilai_b
+
+print("Hasil Pertambahann : ", tambah)
+print("Hasil Pengurangan  : ", kurang)
+print("Hasil Perkalian    : ", kali)
+print("Hasil Pembagian    : ", bagi)
+print("Hasil Modulus      : ", modulus)
